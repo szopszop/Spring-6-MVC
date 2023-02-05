21400b1c561ff2d0d315b3be1e61dde4eed3a2af
szopszop <szymontracz1@gmail.com>
2023-02-05 21:04:32 +0100

    added Beer class

---
+++ src/test/java/com/example/spring6mvc/Spring6MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring6mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
