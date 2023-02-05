8a78f4c070fd4655a7f8df612a2ea8df55913d0a
szopszop <szymontracz1@gmail.com>
2023-02-05 21:21:58 +0100

    .gitignore is now working

---
+++ src/main/java/com/example/spring6mvc/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.example.spring6mvc.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/example/spring6mvc/service/BeerService.java
@@ -0,0 +1,13 @@
+package com.example.spring6mvc.service;
+
+import com.example.spring6mvc.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+}
