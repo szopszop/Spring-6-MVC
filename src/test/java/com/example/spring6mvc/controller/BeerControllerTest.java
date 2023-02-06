716759dffe8f2bd0df231a7a5512c258db1dcbf2
szopszop <szymontracz1@gmail.com>
2023-02-06 16:32:18 +0100

    beer tests

--- src/test/java/com/example/spring6mvc/controller/BeerControllerTest.java
+++ src/test/java/com/example/spring6mvc/controller/BeerControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.spring6mvc.controller;
 
+import com.example.spring6mvc.model.Beer;
 import com.example.spring6mvc.service.BeerService;
+import com.example.spring6mvc.service.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,15 +12,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@SpringBootTest
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
-
-//    @Autowired
-//    BeerController beerController;
 
     @Autowired
     MockMvc mockMvc;
@@ -26,11 +26,31 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
-    @Test
-    void getBeerById() throws Exception {
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID())
+    @Test
+    void shouldReturnBeer() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+
+        given(beerService.getBeerById(testBeer.getBeerId())).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + testBeer.getBeerId())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                // $ is root direction
+                .andExpect(jsonPath("$.beerId", is(testBeer.getBeerId().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName())));
     }
+
+    @Test
+    void shouldReturnAllBeers() throws Exception {
+        given(beerService.listBeers()).willReturn(beerServiceImpl.listBeers());
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
 }
