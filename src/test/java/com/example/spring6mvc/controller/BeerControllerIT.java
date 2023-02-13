6bfde9b84a025718e6eebdf122817752e926af79
szopszop <szymontracz1@gmail.com>
2023-02-13 11:47:53 +0100

    password refactoring

--- src/test/java/com/example/spring6mvc/controller/BeerControllerIT.java
+++ src/test/java/com/example/spring6mvc/controller/BeerControllerIT.java
@@ -30,6 +30,7 @@ import static com.example.spring6mvc.controller.BeerController.BEER_PATH_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -59,7 +60,9 @@ class BeerControllerIT {
     @BeforeEach
     void setUp() {
 
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
